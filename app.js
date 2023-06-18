58e95b4ed128cf38b1714bc7c9cddff75cd974c0
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-19 00:10:59 +0100

    commiting.....

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const searchUser = document.getElementById('searchUser');
 
+const github = new GitHub;
+
 searchUser.addEventListener('keyup', (e) =>{
    const userText = e.target.value;
 
    if(userText !== ''){
-    console.log(userText);
+     github.getUser(userText).then(data =>{
+      if(data.profile.message === "Not Found"){
+
+      } else{
+
+     }
+     } )
+   } else{
+    
    }
-})
+});
--- github.js
+++ github.js
@@ -5,5 +5,9 @@ class GitHub{
   }
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+   const profileData = await profileResponse.json();
+   return{
+    profile
+   } 
   }
 }
